1e153c7cfd657c9c93eac38b57fe3787179c296a
GLB7 <124634022+GLB7@users.noreply.github.com>
2025-04-30 18:27:58 -0400

    Part b implemented

--- huffman.py
+++ huffman.py
@@ -31,6 +31,38 @@ def frequency_table(st: str) -> None:
 
 
 # Part b
+import heapq
+class Node:
+    def __init__(self, char, freq):
+        self.char  = char # Leaf nodes = symbol / char | Intenral nodes = None
+        self.freq  = freq
+        self.left  = None
+        self.right = None
+
+    # “<” so heapq can compare nodes (by frequency) when pushing/popping to heapify
+    def __lt__(self, other):
+        return self.freq < other.freq
+    
+    # Uses DFS to get the huffman codes
+    def getHuffmanCodes(self, current_code="", codes=None):
+        # For first instance to initialize the codes dictionary
+        if codes is None:
+            codes = {}
+
+        # Base case (It's a leaf node, an actual character)
+        if self.char is not None:
+            if current_code == "": # If the tree has only one symbol
+                codes[self.char] = "0"
+            else:
+                codes[self.char] = current_code
+        else:
+            if self.left is not None:
+                self.left.getHuffmanCodes(current_code + "0", codes) # Every left traversal is a 0
+            if self.right is not None:
+                self.right.getHuffmanCodes(current_code + "1", codes) # Every right traversal is a 1
+        
+        return codes
+
 def Huffman_code(st: str) -> None:
     """
     Builds a Huffman tree from the string st and generates the Huffman codes for each character. Print each character with its corresponding code.
@@ -41,7 +73,43 @@ def Huffman_code(st: str) -> None:
     Output:
         Print the Huffman code for each character in the string. 
     """
-    print("Huffman Codes:\n'a': 000\n...\n")
+    # Build Table Again
+    frequency_table: dict[str, int] = {}
+    for char in st:
+        if char in frequency_table:
+            frequency_table[char] += 1
+        else:
+            frequency_table[char] = 1
+
+    # Build a heap
+    heap = []
+    for char in frequency_table:
+        freq = frequency_table[char]
+        node = Node(char, freq)
+        heapq.heappush(heap, node)
+    
+    # For testing / learning
+    # A valid min heap has: left child at index 2i + 1 : right child at index 2i + 2
+    print("heap =", [(n.char, n.freq) for n in heap])
+
+    while len(heap) > 1:
+        # Extract 2 smallest frequencies from the heap (left & right)
+        right = heapq.heappop(heap)
+        left  = heapq.heappop(heap)
+        parent = Node(None, right.freq + left.freq) # char is None here for the internal node.
+        parent.right, parent.left = right, left
+        heapq.heappush(heap, parent) # put parent back into the heap with the updated frequency. (heap is always shrinking by 2 and growing by 1)
+        # Visaul of heap being built (also for testing / learning)
+        print("heap =", [(n.char, n.freq) for n in heap])
+
+    root = heapq.heappop(heap) # This is the huffman tree!!!
+    huffman_codes = root.getHuffmanCodes()
+
+    # Print the characters from the frequency table in sorted order (based on char).
+    print("\nHuffman Codes:")
+    for ch in sorted(huffman_codes):
+        print(f"{repr(ch)}: {huffman_codes[ch]}")
+
 # Part c
 def Huffman_encode(st: str, codes: dict) -> None:
     """
@@ -54,8 +122,7 @@ def Huffman_encode(st: str, codes: dict) -> None:
     Output:
         Print the binary-encoded string using the Huffman codes. 
     """
-    print("Encoded String:\n0000010010010101011111\n")
-
+    print("Encoded String:\n")
 
 # Part d
 def Huffman_tree(L: list):
@@ -82,12 +149,20 @@ def Huffman_decode(bst: str, tree: object) -> None:
     Output:
         Prints the decoded string.
     """
-    print("Decoded String:\nabbccdddd")
+    print("Decoded String:\n")
 
 def main():
-    # This string example is from slide 36
-    st = "a fast runner need never be afraid of the dark"
+    
+    st = "abbcccdddd" # Assignment Example
+    st2 = "a fast runner need never be afraid of the dark" # Slides Example
+    print("Part a:\nst = \"" + st + "\"")
     frequency_table(st)
+    print("Part a:\nst = \"" + st2 + "\"")
+    frequency_table(st2)
+    print("Part b:\nst = \"" + st + "\"")
+    Huffman_code(st)
+    print("Part b:\nst = \"" + st2 + "\"")
+    Huffman_code(st2)
 
 if __name__ == "__main__":
     main()
