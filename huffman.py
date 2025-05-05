23f3ca94f4e8cf5fa0d85dac84f6215b707dfdf6
GLB7 <124634022+GLB7@users.noreply.github.com>
2025-05-05 17:27:20 -0400

    Merged version of all attempts for final submission.

--- huffman.py
+++ huffman.py
@@ -11,30 +11,51 @@ def frequency_table(st: str) -> None:
     Output:
         Print each character along with its corresponding frequency.
     """
+    # Example of this initalized table on Greedy Method 1 Slide 36
+    frequency_table: dict[str, int] = {}
 
-    #For this problem we literally cannot do anything that isn't O(n)
-    #The problem does not assume that it contains numeric or any other characters.
-    #For the beginning implementation, let's assume it's just lowercase letters.
-
-    #This simply creates the table
-    freq_table = {};
-
-    for letter in range(26):
-        freq_table[chr(97+letter)] = 0;
-    
-    #Loop through the word
+    # Loop through all characters in given string to fill the frequency table.
+    # If a character already exists in the table increment the count, else make a new entry for it starting at 1.
     for char in st:
-        freq_table[char] += 1
-    
-    #Print Frequencies once done.
-    print("Character Frequencies:\n")
+        if char in frequency_table:
+            frequency_table[char] += 1
+        else:
+            frequency_table[char] = 1
 
-    for key, val in freq_table.items():
-        print('\''+key+'\': '+str(val)+'\n');
-
+    # Print the characters from the frequency table in sorted order (based on char).
+    # repr() function allows for special characters like space to be represented.
+    print("Character Frequencies:")
+    for char in sorted(frequency_table):
+        print(f"{repr(char)}: {frequency_table[char]}")
 
 # Part b
-def Huffman_code(st: str) -> None:
+import heapq
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None, c=''):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.c = c
+    
+    # “<” so heapq can compare nodes (by frequency) when pushing/popping to heapify
+    def __lt__(self, other):
+        return self.val < other.val
+
+def inorderTraversal(root, encoding, codes=None):
+    if codes is None:
+            codes = {}
+
+    if(root==None):
+        return 
+    inorderTraversal(root.left, encoding+"0", codes)
+    if root.c:
+        print("\'" + root.c + "\' " , ":", encoding)
+        codes[root.c] = encoding
+    inorderTraversal(root.right, encoding+"1", codes)
+    return codes
+
+def Huffman_code(st: str):
     """
     Builds a Huffman tree from the string st and generates the Huffman codes for each character. Print each character with its corresponding code.
 
@@ -44,9 +65,47 @@ def Huffman_code(st: str) -> None:
     Output:
         Print the Huffman code for each character in the string. 
     """
-    print("Huffman Codes:\n'a': 000\n...\n")
+    if st == "":
+        print("Huffman Codes:\nNone")
+        return {}
+    
+    # Make Frequency Table of characters
+    freq_table = {}
+
+    # O(n) where n is length of string
+    for c in st:
+        if c not in freq_table:
+            freq_table[c] = 1
+        else:
+            freq_table[c] +=1
+    
+    # Make a tree node for every character, we want a heap of tree nodes
+    tree_nodes = []
+    for key in freq_table:
+        heapq.heappush(tree_nodes, TreeNode(freq_table[key], c=key))
+
+    while len(tree_nodes)>1:
+        c1 = heapq.heappop(tree_nodes)
+        c2 = heapq.heappop(tree_nodes)
+        
+        # If c1 is new node, add it on the right
+        if c1.c !='' and c2.c =='':
+            heapq.heappush(tree_nodes, TreeNode(c1.val + c2.val, left=c2, right = c1))
+
+        # Else, then c2 is a new node, add it on right
+        else:
+            heapq.heappush(tree_nodes, TreeNode(c1.val + c2.val, left=c1, right = c2))
+    
+    # Last node in tree_nodes is root
+    root = heapq.heappop(tree_nodes)
+
+    # Traverse the tree to print huffman codes for every character
+    huffman_codes = inorderTraversal(root, "")
+    return huffman_codes
+    
+
 # Part c
-def Huffman_encode(st: str, codes: dict) -> None:
+def Huffman_encode(st: str, codes: dict):
     """
     Prints the binary encoded version of st, based on the Huffman codes generated in part (b) codes.
 
@@ -57,10 +116,21 @@ def Huffman_encode(st: str, codes: dict) -> None:
     Output:
         Print the binary-encoded string using the Huffman codes. 
     """
-    print("Encoded String:\n0000010010010101011111\n")
+    encoded = ""
+    for char in st:
+        encoded += codes[char]
+    print("Encoded String:")
+    print(encoded)
+    return encoded # To use in part e as bst
 
 
 # Part d
+class unknownHuffmanNode:
+    def __init__(self):
+        self.char = None
+        self.left = None
+        self.right = None
+
 def Huffman_tree(L: list):
     """
     Given a list L of characters and their corresponding Huffman codes; construct a Huffman tree based on this list.
@@ -71,7 +141,25 @@ def Huffman_tree(L: list):
     Output:
         Return the Huffman tree constructed from the list. 
     """
-    pass
+
+    root = unknownHuffmanNode()
+
+    for char, code in L:
+        node = root
+        for path in code:
+            if path == '0':
+                if not node.left:
+                    node.left = unknownHuffmanNode()
+                node = node.left
+            elif path == '1':
+                if not node.right:
+                    node.right = unknownHuffmanNode()
+                node = node.right
+            else:
+                raise ValueError("Huffman codes should be binary!")
+        node.char = char 
+
+    return root
 
 # Part e
 def Huffman_decode(bst: str, tree: object) -> None:
@@ -85,18 +173,85 @@ def Huffman_decode(bst: str, tree: object) -> None:
     Output:
         Prints the decoded string.
     """
-    print("Decoded String:\nabbccdddd")
+    decoded_string = ""
+
+    root = tree
+    node = root
+
+    print("Decoded String:")
+    # Traverse the huffman tree to decode each bit in bst until it reaches a leaf node which will be a character.
+    for path in bst:
+        if path == '0':
+            node = node.left
+        elif path == '1':
+            node = node.right
+        else: 
+            raise ValueError("Huffman codes should be binary!")
+        
+        if not node.left and not node.right:
+            decoded_string += node.char
+            node = root
+    
+    print(decoded_string)
 
 def main():
-    st = "abbcccdddd"
+    
+    st = "abbcccdddd" # Assignment Example
+    st2 = "a fast runner need never be afraid of the dark" # Slides Example
+    st3 = "" # Edge Case
+    
+    print("Part a:\nst = \"" + st + "\"")
     frequency_table(st)
-    codes = {"a": "000", "b": "001", "c": "01", "d": "1"}
-    Huffman_code(st)
-    Huffman_encode(st, codes)
-    L = [('a', '000'), ('b', '001'), ('c', '01'), ('d', '1')]
-    tree = Huffman_tree(L)
-    bst = "0000010010010101011111"
-    Huffman_decode(bst, tree)
+    print()
+    print("Part a:\nst = \"" + st2 + "\"")
+    frequency_table(st2)
+    print()
+    print("Part a:\nst = \"" + st3 + "\"")
+    frequency_table(st3)
+    print()
+    
+    print()
+
+    print("Part b:\nst = \"" + st + "\"")
+    st_code = Huffman_code(st)
+    print()
+    print("Part b:\nst = \"" + st2 + "\"")
+    st2_code = Huffman_code(st2)
+    print()
+    print("Part b:\nst = \"" + st3 + "\"")
+    st3_code = Huffman_code(st3)
+    print()
+
+    print()
+
+    print("Part c:\nst = \"" + st + "\"")
+    st_encoded = Huffman_encode(st, st_code)
+    print()
+    print("Part c:\nst = \"" + st2 + "\"")
+    st2_encoded = Huffman_encode(st2, st2_code)
+    print()
+    print("Part c:\nst = \"" + st3 + "\"")
+    st3_encoded = Huffman_encode(st3, st3_code)
+    print()
+
+    print()
+
+    print("part d & e:\nst = \"" + st + "\"")
+    codes_list = [('a', '000'), ('b', '001'), ('c', '01'), ('d', '1')]
+    tree = Huffman_tree(codes_list)
+    Huffman_decode(st_encoded, tree)
+    print()
+
+    print("part d & e:\nst = \"" + st2 + "\"")
+    codes_list2 = [('r', '000'), ('a', '001'), (' ', '01'), ('t', '1000'), ('f', '1001'), ('e', '101'), ('s', '110000'), ('u', '110001'), ('o', '110010'), ('h', '110011'), ('d', '1101'), ('b', '111000'), ('i', '111001'), ('v', '111010'), ('k', '111011'), ('n', '1111')]
+    tree2 = Huffman_tree(codes_list2)
+    Huffman_decode(st2_encoded, tree2)
+    print()
+
+    print("part d & e:\nst = \"" + st3 + "\"")
+    codes_list3 = []
+    tree3 = Huffman_tree(codes_list3)
+    Huffman_decode(st3_encoded, tree3)
 
 if __name__ == "__main__":
     main()
